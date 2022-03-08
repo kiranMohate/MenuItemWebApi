4445a34792a8d5c96af3d75a8aa52093d4849d1c
kiranMohate <49229870+kiranMohate@users.noreply.github.com>
2022-03-08 11:57:49 +0530

    first commit

---
+++ Controllers/MenuItemController.cs
@@ -0,0 +1,36 @@
+﻿using MenuItemListingWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MenuItemListingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        List<MenuItem> menuItems;
+        MenuItem i1;
+        public MenuItemController()
+        {
+            i1 = new MenuItem { Id = 1, Name = "pizza", freeDelivery = true, Price = 200, dateOfLaunch = Convert.ToDateTime("2/2/2030"), Active = true };
+            menuItems = new List<MenuItem>()
+            {
+                new MenuItem(){Id=1,Name="pizza",freeDelivery=true,Price=200,dateOfLaunch=Convert.ToDateTime("2/2/2030"),Active=true},
+                 new MenuItem(){Id=2,Name="pizza1",freeDelivery=false,Price=100,dateOfLaunch=Convert.ToDateTime("2/2/2030"),Active=true},
+                  new MenuItem(){Id=3,Name="pizza2",freeDelivery=true,Price=400,dateOfLaunch=Convert.ToDateTime("1/2/2030"),Active=false}
+            };
+        }
+        [HttpGet]
+        public ActionResult<List<MenuItem>> Get()
+        {
+            
+            return menuItems;
+        }
+        [HttpGet("id")]
+        public ActionResult<MenuItem> GetById(int id)
+        {
+            var i2=menuItems.FirstOrDefault(mi => mi.Id == id);
+            return i2;
+        }
+    }
+}
